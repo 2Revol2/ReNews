8cb80654dc9b07b334c49798c5065cf9382f0c2b
2Revol2 <maksimbaluta02@gmail.com>
2025-01-29 12:25:24 +0300

    feat: improve Skeleton UI on mobile

--- src/shared/ui/Skeleton/Skeleton.tsx
+++ src/shared/ui/Skeleton/Skeleton.tsx
@@ -1,12 +1,14 @@
 import ContentLoader from "react-content-loader";
 import s from "./Skeleton.module.scss";
 import { useTheme } from "@/app/provides/ThemeProvider";
+import { useMobile } from "@/shared/lib/hooks/useMobile";
 
 interface SkeletonProps {
   count: number;
 }
 export const Skeleton = ({ count }: SkeletonProps) => {
   const { isDark } = useTheme();
+  const isMobile = useMobile(600);
   return (
     <ul className={s.list}>
       {[...new Array(count)].map((_, index) => {
@@ -14,14 +16,23 @@ export const Skeleton = ({ count }: SkeletonProps) => {
           <ContentLoader
             key={index}
             speed={2}
-            width={250}
-            height={295}
-            viewBox="0 0 250 295"
+            width={isMobile ? "100%" : "250"}
+            height={isMobile ? "487" : "250"}
+            viewBox={`0 0 ${isMobile ? "100%" : "250"} ${
+              isMobile ? "487" : "250"
+            }`}
             backgroundColor={isDark ? "#262b32" : "#c9c9c9"}
             foregroundColor="#bababa"
             className={s.skeleton}
           >
-            <rect x="-4" y="2" rx="0" ry="0" width="250" height="295" />
+            <rect
+              x="-4"
+              y="2"
+              rx="0"
+              ry="0"
+              width={isMobile ? "100%" : "250"}
+              height={isMobile ? "487" : "250"}
+            />
           </ContentLoader>
         );
       })}
